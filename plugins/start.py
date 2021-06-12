8d7dfb5338d6305826eddcc85ce98e583af3ced3
lolyz <68340411+lolyz@users.noreply.github.com>
2021-06-12 17:28:14 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,11 +93,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>Kamu Harus Bergabung / Subscribe Channel Telegram Saya\n\n\n\n\n\nKlik Coba Lagi Kalau Sudah Bergabung Channel\n\n</b>"
+    text = "<b>Kamu Harus Bergabung / Subscribe Channel Telegram Saya\n\n\n\nKlik Coba Lagi Kalau Sudah Bergabung Channel</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>Coba Lagi</a></b>"
+        text = text + f" <b>\n\n<a href='https://t.me/{client.username}?start={argument}'>COBA LAGI</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])

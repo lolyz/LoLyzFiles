41dfab1e395fc384eda962e9908500b506282301
lolyz <68340411+lolyz@users.noreply.github.com>
2021-06-12 17:22:37 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -101,7 +101,6 @@ async def not_joined(client: Client, message: Message):
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("DONE", url = https://t.me/{client.username}?start={argument})]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,

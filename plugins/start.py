6a4610839fdaa5276577fd53a1088572ac7db71c
lolyz <68340411+lolyz@users.noreply.github.com>
2021-09-02 09:29:20 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -107,7 +107,7 @@ async def not_joined(client: Client, message: Message):
                        InlineKeyboardButton("𝐓𝐄𝐑𝐕𝐈𝐑𝐀𝐋", url="https://t.me/TerViral")
                   ],
                   [
-                       InlineKeyboardButton("🔄 𝐆𝐄𝐓 𝐅𝐈𝐋𝐄 🔄", url=f"https://t.me/{client.username}?start={argument}")
+                       InlineKeyboardButton("🔄 𝐆𝐄𝐓 𝐅𝐈𝐋𝐄 🔄", url=f'https://t.me/{client.username}?start={argument}')
                   ]
              ]
          )
